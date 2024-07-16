fe45f9dcd9d992a357d411dab6865435f8c96623
Ravidu Perera <sheahn20133@gmail.com>
2024-07-16 20:24:07 +0530

    Add more extracted contents

--- pdf_list.py
+++ pdf_list.py
@@ -1,12 +1,35 @@
 pdf_list=[
-            'Dataset/raw_pdf/1450.pdf',
-            'Dataset/raw_pdf/1458.pdf',
-            'Dataset/raw_pdf/1460.pdf',
-            'Dataset/raw_pdf/6203.pdf',
-            'Dataset/raw_pdf/6323.pdf',
-            'Dataset/raw_pdf/6325.pdf',
-            'Dataset/raw_pdf/6332.pdf',
-            'Dataset/raw_pdf/6343.pdf',
-            'Dataset/raw_pdf/6344.pdf',
+            'Dataset/raw_pdf/1411.pdf',
+            'Dataset/raw_pdf/1453.pdf',
+            'Dataset/raw_pdf/1485.pdf',
+            'Dataset/raw_pdf/6289.pdf',
+            'Dataset/raw_pdf/6305.pdf',
+            'Dataset/raw_pdf/6310.pdf',
+            'Dataset/raw_pdf/6311.pdf',
+            'Dataset/raw_pdf/6317.pdf',
+            'Dataset/raw_pdf/6318.pdf',
+            'Dataset/raw_pdf/6319.pdf',
+            'Dataset/raw_pdf/6321.pdf',
+            'Dataset/raw_pdf/6322.pdf',
+            'Dataset/raw_pdf/6327.pdf',
+            'Dataset/raw_pdf/6330.pdf',
+            'Dataset/raw_pdf/6333.pdf',
+            'Dataset/raw_pdf/6334.pdf',
+            'Dataset/raw_pdf/6336.pdf',
+            'Dataset/raw_pdf/6307.pdf',
             # Add more file paths as needed
         ]
+
+
+# [
+#             'Dataset/raw_pdf/1450.pdf',
+#             'Dataset/raw_pdf/1458.pdf',
+#             'Dataset/raw_pdf/1460.pdf',
+#             'Dataset/raw_pdf/6203.pdf',
+#             'Dataset/raw_pdf/6323.pdf',
+#             'Dataset/raw_pdf/6325.pdf',
+#             'Dataset/raw_pdf/6332.pdf',
+#             'Dataset/raw_pdf/6343.pdf',
+#             'Dataset/raw_pdf/6344.pdf',
+#             # Add more file paths as needed
+#         ]
