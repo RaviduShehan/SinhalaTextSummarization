47d95ab3e48e754d05d02766aacb1719e893511e
Ravidu Perera <sheahn20133@gmail.com>
2024-07-16 11:49:39 +0530

    Add more extracted contents

---
+++ pdf_list.py
@@ -0,0 +1,12 @@
+pdf_list=[
+            'Dataset/raw_pdf/1450.pdf',
+            'Dataset/raw_pdf/1458.pdf',
+            'Dataset/raw_pdf/1460.pdf',
+            'Dataset/raw_pdf/6203.pdf',
+            'Dataset/raw_pdf/6323.pdf',
+            'Dataset/raw_pdf/6325.pdf',
+            'Dataset/raw_pdf/6332.pdf',
+            'Dataset/raw_pdf/6343.pdf',
+            'Dataset/raw_pdf/6344.pdf',
+            # Add more file paths as needed
+        ]
